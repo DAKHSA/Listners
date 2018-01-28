ee5645a00f926a45bce1fd2ceab762aa36e70489
DAKSHA <shivubd@gmail.com>
2018-01-28 16:36:55 +0530

    Listner scripts

---
+++ src/test/java/Listners/DemoListner.java
@@ -0,0 +1,24 @@
+package Listners;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoListner {
+	
+	@Test
+	public void test1() {
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void test2() {
+		Assert.assertEquals(false, true);
+	}
+
+	@Test
+	public void test3() {
+		Assert.assertEquals(false, true);
+	}
+}
